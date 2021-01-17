08422cdf4ef1c1c1da736c1435888872f89716bd
Vladislav Arsenev <forjavatests8@gmail.com>
2021-01-17 10:30:52 +0500

    Add swagger2

--- src/main/java/com/arsenev/employees/web/RootController.java
+++ src/main/java/com/arsenev/employees/web/RootController.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Перенаправляет из корня рута (localhost:8080) в view с главной страницей.
+ * Аннотация ApiIgnore для того, чтобы Swagger не заносил в документацию данный контроллер.
  */
+@ApiIgnore
 @Controller
 public class RootController {
     private static final Logger LOG = LoggerFactory.getLogger(RootController.class);
